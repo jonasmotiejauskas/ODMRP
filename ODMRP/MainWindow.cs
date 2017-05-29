62330a1d7938967d4ab367b731983909b53660b8
Jonas M <jonasmotiejauskas@gmail.com>
2017-05-29 22:36:04 +0300

    Interface being done

---
+++ ODMRP/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ODMRP
+{
+    public partial class MainWindow : Form
+    {
+        List<ODMRPelements.Node> nodeList = new List<ODMRPelements.Node>();
+        UIelements.NetworkPanel panel = new UIelements.NetworkPanel() { BackColor = Color.LightGray, Size = new Size(700, 500), Location = new Point(30, 140), BorderStyle = BorderStyle.FixedSingle };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Controls.Add(panel);
+        }
+
+    }
+}
---
+++ ODMRP/ODMRPelements/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODMRP.ODMRPelements
+{
+    class Node
+    {
+        public double NodeId { get; set; }
+        public double Coordinate_X { get; set; }
+        public double Coordinate_Y { get; set; }
+        public double Range { get; set; }
+    }
+}
